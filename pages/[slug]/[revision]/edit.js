51a2b7b7ad703a1289e51672ea9dc8243326c12a
Stuart Rutter <stuart.rutter@metro.co.uk>
2022-11-17 15:28:20 +0000

    edit, preview noindex

--- pages/[slug]/[revision]/edit.js
+++ pages/[slug]/[revision]/edit.js
@@ -1,13 +1,24 @@
+import Head from 'next/head'
+
 import { pagesCollection } from '../../../lib/mongodb'
 import EditForm from '../../../components/forms/Edit'
 import Layout from '../../../components/Layout'
 
 export default function Edit({pageData}) {
   return (
-    <Layout>
-      <div className="py-2"></div>
-      <EditForm pageData={pageData} />
-    </Layout>
+    <>
+      <Head>
+        <meta 
+          key="robots" 
+          name="robots" 
+          content="noindex,follow" 
+        />
+      </Head>
+      <Layout>
+        <div className="py-2"></div>
+        <EditForm pageData={pageData} />
+      </Layout>
+    </>
   )
 }
 
--- pages/[slug]/[revision]/preview.js
+++ pages/[slug]/[revision]/preview.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import { pagesCollection } from '../../../lib/mongodb'
 import Router from 'next/router'
 import { signIn, useSession } from "next-auth/react"
@@ -60,55 +62,64 @@ export default function Preview(props) {
   }
 
   return (
-    <Layout>
-      <hgroup>
-        <h1 className="text-2xl py-6 font-bold">{title}</h1>
-      </hgroup>
-      <section>
-        <Meta pageData={props.pageData} />
-      </section>
-      <hr className="my-5" />
-      {info &&
+    <>
+      <Head>
+        <meta 
+          key="robots" 
+          name="robots" 
+          content="noindex,follow" 
+        />
+      </Head>
+      <Layout>
+        <hgroup>
+          <h1 className="text-2xl py-6 font-bold">{title}</h1>
+        </hgroup>
         <section>
-          <Info info={info} />
+          <Meta pageData={props.pageData} />
         </section>
-      }
-      {initHTML &&
-        <section>
-          <PrepCode prepCode={initHTML} />
-        </section>
-      }
-      {setup &&
-        <section>
-          <Setup setup={setup} />
-        </section>
-      }
-      {teardown &&
-        <section>
-          <Teardown teardown={teardown} />
-        </section>
-      }
-      <section>
-        <TestRunner id={_id} tests={tests} />
-      </section>
-      <hr className="my-5" />
-      <div className="flex justify-end">
-        { canEdit &&
-            <>
-              <a href={`/${slug}/${revision}/edit`} className={buttonStyles.default}>Edit Tests</a><span className="inline-flex items-center px-2"> - or - </span>
-            </>
+        <hr className="my-5" />
+        {info &&
+          <section>
+            <Info info={info} />
+          </section>
         }
-        { !session &&
-            <button className="bg-gray-100 hover:bg-gray-200 text-gray-darkest font-bold py-1 px-2 rounded inline-flex items-center border border-gray-400" type="button" onClick={() => signIn("github")}>
-              <GitHubIcon fill="#000000" width={32} height={32} className="mr-2" />
-              <span>Login with GitHub to Publish</span>
-            </button>
+        {initHTML &&
+          <section>
+            <PrepCode prepCode={initHTML} />
+          </section>
         }
-        { canPublish &&
-            <a onClick={publish} href="#" className={styles.unpublishedButton}>Publish</a> 
+        {setup &&
+          <section>
+            <Setup setup={setup} />
+          </section>
         }
-      </div>
-    </Layout>
+        {teardown &&
+          <section>
+            <Teardown teardown={teardown} />
+          </section>
+        }
+        <section>
+          <TestRunner id={_id} tests={tests} />
+        </section>
+        <hr className="my-5" />
+        <div className="flex justify-end">
+          { canEdit &&
+              <>
+                <a href={`/${slug}/${revision}/edit`} className={buttonStyles.default}>Edit Tests</a><span className="inline-flex items-center px-2"> - or - </span>
+              </>
+          }
+          { !session &&
+              <button className="bg-gray-100 hover:bg-gray-200 text-gray-darkest font-bold py-1 px-2 rounded inline-flex items-center border border-gray-400" type="button" onClick={() => signIn("github")}>
+                <GitHubIcon fill="#000000" width={32} height={32} className="mr-2" />
+                <span>Login with GitHub to Publish</span>
+              </button>
+          }
+          { canPublish &&
+              <a onClick={publish} href="#" className={styles.unpublishedButton}>Publish</a> 
+          }
+        </div>
+      </Layout>
+    </>
   )
 }
 
