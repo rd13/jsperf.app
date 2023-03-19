5907d4dde4660e6d6a506c3496f9293a84feb6b2
Stuart Rutter <stuart.rutter@metro.co.uk>
2023-03-19 16:31:03 +0000

    fixes for deferred

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-// https://github.com/bestiejs/benchmark.js/issues/128#issuecomment-271615298
 module.exports = {
-  webpack: (config, options) => {
-    config.module.noParse = [
-      /benchmark/
-    ]
-    return config
-  },
   transpilePackages: ['codejar']
 }
