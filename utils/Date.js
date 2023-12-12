277b2f75ba0281c98b7f541bc3e5953ce2feed5d
Stuart Rutter <stuart.rutter@metro.co.uk>
2023-12-12 12:18:23 +0000

    fix: date locale string

--- utils/Date.js
+++ utils/Date.js
@@ -1,13 +1,3 @@
-import { useState, useEffect } from "react"
-
-export const datetimeLong = (date) => {
-  const [formattedDate, setFormattedDate] = useState(null)
-
-  useEffect(
-    () => setFormattedDate(new Date(date).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })),
-    []
-  )
-
-  return formattedDate
+export const datetimeLong = date => {
+  return (new Date(date)).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })
 }
-
