ceea60cd1d0d6d53c7e0d17f5476f18f5fc2e190
Stuart Rutter <stuart.rutter@dmgmedia.tech>
2025-06-05 21:48:25 +0100

    feat: add language to code block

--- components/sections/Setup.js
+++ components/sections/Setup.js
@@ -6,7 +6,7 @@ export default function Setup(props) {
     <>
       <h2 className="font-bold my-5">Setup</h2>
       <pre className="max-h-80 overflow-scroll">
-        <code dangerouslySetInnerHTML={{__html: highlightSanitizedJS(setup)}} />
+        <code className="language-javascript" dangerouslySetInnerHTML={{__html: highlightSanitizedJS(setup)}} />
       </pre>
     </>
   )
