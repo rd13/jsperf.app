317a7f891467a062a3cecc8b302f1053615b862b
Stuart Rutter <stuart.rutter@dmgmedia.tech>
2025-08-22 09:58:38 +0100

    fix: rm baseUrl

--- app/lib/auth-client.js
+++ app/lib/auth-client.js
@@ -1,5 +1,2 @@
 import { createAuthClient } from "better-auth/react"
-export const { signIn, signOut, useSession } = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
-})
+export const { signIn, signOut, useSession } = createAuthClient({})
